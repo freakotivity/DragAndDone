b9a3cdc3e9860b006d99bcf064ab1a866a10221e
Gabriel Kroll <gabriel.kroll@me.com>
2015-02-03 22:29:10 +0100

    swiftify function problem

--- DragAndDone/TaskView.swift
+++ DragAndDone/TaskView.swift
@@ -17,7 +17,7 @@ class TaskView: UIView {
     var doneIndex:Int!
     var textLabel:UILabel!
     var image:UIImage?
-
+    
     override func drawRect(rect: CGRect) {
         let lineWidth:CGFloat = 10.0
         let square = CGRectMake(0, 0, min(rect.size.width, rect.size.height), min(rect.size.width, rect.size.height))
@@ -30,22 +30,52 @@ class TaskView: UIView {
         {
             image!.drawInRect(rect)
         }
-
+        
         CGContextAddEllipseInRect(context, CGRectInset(square, lineWidth, lineWidth))
         CGContextStrokePath(context)
         
     }
+    
+//    func convertImageToGrayScale(image: UIImage) -> UIImage {
+//    
+//    // Create image rectangle with current image width/height
+//    let imageRect = CGRectMake(0, 0, image.size.width, image.size.height)
+//    
+//    // Grayscale color space
+//    let colorSpace = CGColorSpaceCreateDeviceGray()
+//    
+//    // Create bitmap content with current image size and grayscale colorspace
+//    let context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone)
+//    
+//    // Draw image into current context, with specified rectangle
+//    // using previously defined context (with grayscale colorspace)
+//    CGContextDrawImage(context, imageRect, image.CGImage)
+//    
+//    // Create bitmap image info from pixel data in current context
+//    let imageRef = CGBitmapContextCreateImage(context)
+//    
+//    // Create a new UIImage object
+//        let newImage : UIImage = imageWithCGImage(imageRef)
+//    
+//    // Release colorspace, context and bitmap information
+//    CGColorSpaceRelease(colorSpace);
+//    CGContextRelease(context);
+//    CFRelease(imageRef);
+//    
+//    // Return the new grayscale image
+//    return newImage
+//    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         println("TASK VIEW INIT WITH FRAME")
         self.textLabel = UILabel(frame: frame)
         self.textLabel.backgroundColor = UIColor.clearColor()
-//        self.textLabel.text = "YEAH"
+        //        self.textLabel.text = "YEAH"
         self.textLabel.textAlignment = NSTextAlignment.Center
         self.addSubview(self.textLabel)
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
