9ce14d73f30bf2a04efe4b21873520b613e0ba3c
Ricardo Gonzalez <ricardo76@me.com>
2015-02-10 08:26:50 +0100

    =)

--- DragAndDone/ViewController.swift
+++ DragAndDone/ViewController.swift
@@ -43,10 +43,12 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
         
         let hamburger = UIButton(frame: CGRectMake(8, 20, 22, 22))
         hamburger.setImage(UIImage(named: "menu burger navbar"), forState: UIControlState.Normal)
+        hamburger.addTarget(self, action: "showEditor:", forControlEvents: UIControlEvents.TouchUpInside)
         self.topBar.addSubview(hamburger)
         
         let statsButton = UIButton(frame: CGRectMake(self.view.frame.size.width - 30, 20, 22, 22))
         statsButton.setImage(UIImage(named: "statistics circle navbar"), forState: UIControlState.Normal)
+        statsButton.addTarget(self, action: "checkStats:", forControlEvents: UIControlEvents.TouchUpInside)
         self.topBar.addSubview(statsButton)
         
         UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
